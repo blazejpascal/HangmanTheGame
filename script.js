c4e18ef76d3e61fd8374b8261994b41a136e20e4
Stopczyk <you@example.com>
2017-08-01 17:17:16 +0200

    still trials

--- script.js
+++ script.js
@@ -10,15 +10,26 @@ var app = new Vue({
 var app2 = new Vue({
 el: '#app-2',
 data: {
-  contaningArray: app.hangmanWord.split(""),
+  contaningArray: app.hangmanWord.split(""),//TODO jak stworzyc tablece
+
+  cover: {
+  firstPosition: 'X',
+    for (i=0; i<this.contaningArray.length; i++) {
+      var leter
+
+    }
 
 },
+  userLetter: 'Insert your letter'
+},
 methods: {
-/*  splitArray: function() {
-    for (var i=0; i< contaningArray.length; i++) {
-      console.log(contaningArray[i]);
-    }
-  }*/
+  splitArray: function() {
+    for(var i = 0;i< this.contaningArray.length; i++)
+      if (this.userLetter == this.contaningArray[i]) {
+        console.log('working');
+        this.cover.firstPosition = this.contaningArray[i];
+      }
+  }
 }
 
 });
